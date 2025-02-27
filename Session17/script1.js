9e89b749486a79019eac2a130089a9fb2ce6dd2e
Zaira98742 <ngducanh98742@gmail.com>
2025-02-27 10:28:00 +0700

    Add files via upload

---
+++ Session17/script1.js
@@ -0,0 +1,6 @@
+// Use the following to include this JavaScript file 
+// <script src="script1.js"></script>
+let name=prompt("nhap ten");
+let age=prompt("nhap tuoi");
+console.log("ten cua ban la: ",name);
+console.log("tuoi cua ban la: ",age);
