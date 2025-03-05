48c6535e976b6b22d5e89ebb34806493204613b2
Zaira98742 <ngducanh98742@gmail.com>
2025-03-05 10:22:09 +0700

    Add files via upload

---
+++ Session21/script5.js
@@ -0,0 +1,9 @@
+for(let i=0;i<100;i++){
+  if(i%3==0&& i%5==0){
+    document.write("fizzBuzz<br>");
+  }else if(i%5==0){
+    document.write("buzz<br>");
+  }else if(i%3==0){
+    document.write("fizz<br>");
+}
+}
