656f2e3092fdf65a03fc9864900270739d23c984
Zaira98742 <ngducanh98742@gmail.com>
2025-03-05 21:59:05 +0700

    Add files via upload

---
+++ Session21/script12.js
@@ -0,0 +1,4 @@
+for (let i = 0; i < 10; i++) {
+    let color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    console.log(`mau sac da doi`, `color: ${color}`);
+}
