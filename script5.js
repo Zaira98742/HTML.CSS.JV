02c29ae62936138845ffb2688bfd395064de6a35
Zaira98742 <ngducanh98742@gmail.com>
2025-03-05 10:21:52 +0700

    Delete script5.js

--- script5.js
+++
@@ -1,9 +0,0 @@
-for(let i=0;i<100;i++){
-  if(i%3==0&& i%5==0){
-    document.write("fizzBuzz<br>");
-  }else if(i%5==0){
-    document.write("buzz<br>");
-  }else if(i%3==0){
-    document.write("fizz<br>");
-}
-}
